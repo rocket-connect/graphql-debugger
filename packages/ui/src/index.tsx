d27429ed6da66248c92cb2c07e236c4f83ad5396
danstarns <danielstarns@hotmail.com>
2023-08-28 15:19:23 +0100

    refactor: seperate files frontend

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,10 +1,10 @@
-import { createRoot } from 'react-dom/client';
 import './index.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
+import 'tailwindcss/tailwind.css';
+import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
 const root = createRoot(document.querySelector('#root'));
