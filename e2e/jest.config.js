e9b15f6692e8670bcdbbccd1f3eea4880ae2fcb0
danstarns <danielstarns@hotmail.com>
2023-09-03 17:11:07 +0100

    feat: add init groundwork for e2e tests

---
+++ e2e/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  modulePathIgnorePatterns: ['<rootDir>/build/', '<rootDir>/node_modules/', '<rootDir>/build/'],
+  testTimeout: 15000,
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+    },
+  },
+};
---
+++ e2e/tests/index.test.ts
@@ -0,0 +1,5 @@
+describe('Index', () => {
+  test('should be true', () => {
+    expect(true).toBeTruthy();
+  });
+});
