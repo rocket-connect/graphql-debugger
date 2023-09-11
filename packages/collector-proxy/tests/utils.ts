cd527ab3cd4f3cb68e21a78057a49878cb658d1b
danstarns <danielstarns@hotmail.com>
2023-09-12 00:26:27 +0100

    fix: use correct import for collector app

--- packages/collector-proxy/tests/utils.ts
+++ packages/collector-proxy/tests/utils.ts
@@ -1,6 +1,14 @@
 import supertest from "supertest";
-import { collector } from "../src/collector/app";
+import { collector, postTracesQueue } from "../src/index";
 
 export function request() {
+  postTracesQueue
+    .start()
+    .then(() => {})
+    .catch((e) => {
+      console.log("Failed to start postTracesQueue");
+      console.log(e);
+    });
+
   return supertest(collector);
 }
