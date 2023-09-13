b7697d0cc7d15af7daf54c07e530c0de09ca168b
danstarns <danielstarns@hotmail.com>
2023-09-13 15:38:19 +0100

    types: ignore the generate file

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -2,4 +2,6 @@ export * from "./collector";
 export * from "./entities";
 
 // Generated types from graphql schema
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - this is generated code
 export * as graphql from "./graphql-types";
