fa3819b18b60f6d20432f73131878daf1787ea75
danstarns <danielstarns@hotmail.com>
2023-09-28 17:55:57 +0100

    landing-page: fix intro to be more responsive

--- apps/landing-page/app/src/components/pages/Home.tsx
+++ apps/landing-page/app/src/components/pages/Home.tsx
@@ -3,17 +3,17 @@ import { npxGraphQLDebugger } from "../utils/images";
 
 export function Home() {
   return (
-    <div className="bg-graphiql-medium text-graphiql-light py-20">
+    <div className="bg-graphiql-medium text-graphiql-light py-10">
       <Container>
-        <div className="flex align-center justify-center py-20 w-4/6 mx-auto">
-          <div className="flex flex-col">
-            <h1 className="text-left font-bold text-rocket-connect-darkblue text-6xl tracking-wide">
+        <div className="flex flex-col md:flex-row gap-20 align-center justify-center py-20 w-full md:w-5/6 xl:w-4/6 mx-auto">
+          <div className="flex flex-col gap-20">
+            <h1 className="text-left font-bold text-rocket-connect-darkblue text-center md:text-left mx-auto md:m-0 text-4xl xl:text-5xl tracking-wide">
               Debug your GraphQL server locally.
             </h1>
 
             <a
               href="https://github.com/rocket-connect/graphql-debugger"
-              className="mt-10"
+              className="mx-auto md:m-0"
             >
               <button
                 type="button"
@@ -23,8 +23,7 @@ export function Home() {
               </button>
             </a>
           </div>
-
-          <div className="w-80 my-auto">
+          <div className="w-40 md:w-60 xl:w-80 my-auto mx-auto rotate-6	">
             <img src={npxGraphQLDebugger} alt="npx graphql-debugger" />
           </div>
         </div>
