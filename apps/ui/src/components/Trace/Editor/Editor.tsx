cdf9ed9335f5de576871a315762386c69e85e1a0
Kreshnik Gashi <kreshnnikgashi@gmail.com>
2023-10-06 03:08:57 +0200

    fix(ui): space between editor and trace

--- apps/ui/src/components/Trace/Editor/Editor.tsx
+++ apps/ui/src/components/Trace/Editor/Editor.tsx
@@ -5,7 +5,7 @@ import { Variables } from "./Variables";
 
 export const Editor = ({ trace }: { trace?: Trace }) => {
   return (
-    <div className="flex flex-col h-full justify-between  bg-white-100 divide-y-2 divide-neutral/10  w-5/12 rounded-2xl">
+    <div className="flex flex-col h-full justify-between basis-5/12 bg-white-100 divide-y-2 divide-neutral/10  w-5/12 rounded-2xl">
       <Query trace={trace} />
       <Variables trace={trace} />
     </div>
--- apps/ui/src/components/Trace/TraceStats/TraceStats.tsx
+++ apps/ui/src/components/Trace/TraceStats/TraceStats.tsx
@@ -3,7 +3,7 @@ import { TraceViewer } from "./TraceViewer";
 
 export const TraceStats = () => {
   return (
-    <div className="flex flex-col w-full gap-8 h-full">
+    <div className="flex flex-col basis-8/12 w-full gap-8 h-full">
       <TraceViewer />
       <SchemaTraces />
     </div>
