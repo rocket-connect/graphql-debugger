dbeed03224cf751e20cc0c8e2ca28b3fbdd3dcdb
danstarns <danielstarns@hotmail.com>
2023-10-08 20:00:12 +0100

    fix: spinner time

--- apps/ui/src/utils/sleep.ts
+++ apps/ui/src/utils/sleep.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_SLEEP_TIME = 500; // add delay to all requests for UI
+export const DEFAULT_SLEEP_TIME = 800; // add delay to all requests for UI
 
 export function sleep(time: number) {
   return new Promise((resolve) => {
