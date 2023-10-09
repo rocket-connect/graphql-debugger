f0f556c11e1077172685c3476951c4b6cbf44e1f
danstarns <danielstarns@hotmail.com>
2023-10-09 22:17:14 +0100

    fix: cli message and dev scripts for running cli locally

--- packages/graphql-debugger/src/index.ts
+++ packages/graphql-debugger/src/index.ts
@@ -1,12 +1,41 @@
 #!/usr/bin/env node
+import { BACKEND_PORT } from "@graphql-debugger/backend";
+
 import { SpawnOptionsWithoutStdio, spawn } from "child_process";
 import path from "path";
+
+const COLLECTOR_PATH = process.env.COLLECTOR_PATH
+  ? path.join(__dirname, process.env.COLLECTOR_PATH)
+  : path.join(
+      __dirname,
+      "../",
+      "../",
+      "@graphql-debugger",
+      "backend",
+      "build",
+      "main.js",
+    );
+
+const BACKEND_PATH = process.env.BACKEND_PATH
+  ? path.join(__dirname, process.env.BACKEND_PATH)
+  : path.join(
+      __dirname,
+      "../",
+      "../",
+      "@graphql-debugger",
+      "collector-proxy",
+      "build",
+      "main.js",
+    );
+const childPids: number[] = []; // <-- Create a list to hold the child process PIDs.
 
 function createChildProcess(
   file: string,
   options?: SpawnOptionsWithoutStdio,
 ): Promise<void> {
   const child = spawn("node", [file], options);
+
+  childPids.push(child.pid as number);
 
   child.stdout.pipe(process.stdout);
   child.stderr.pipe(process.stderr);
@@ -25,35 +54,27 @@ function createChildProcess(
 async function main() {
   try {
     const childProcesses = [
-      createChildProcess(
-        path.join(
-          __dirname,
-          "../",
-          "../",
-          "@graphql-debugger",
-          "backend",
-          "build",
-          "main.js",
-        ),
-      ),
-      createChildProcess(
-        path.join(
-          __dirname,
-          "../",
-          "../",
-          "@graphql-debugger",
-          "collector-proxy",
-          "build",
-          "main.js",
-        ),
-        {
-          env: {
-            ...process.env,
-            TRACE_PRISMA: undefined,
-          },
+      createChildProcess(COLLECTOR_PATH),
+      createChildProcess(BACKEND_PATH, {
+        env: {
+          ...process.env,
+          TRACE_PRISMA: undefined,
         },
-      ),
+      }),
     ];
+
+    const messages = [
+      "Thanks for downloading GraphQL Debugger!",
+      "You can use GraphQL Debugger to debug your GraphQL server locally.",
+      "Visit https://www.graphql-debugger.com for more info.",
+      `Debugger Online http://localhost:${BACKEND_PORT}`,
+    ];
+
+    console.log(
+      "\x1b[32m",
+      messages.map((message) => `\n\n${message}`).join(""),
+      "\x1b[0m",
+    );
 
     await Promise.all(childProcesses);
   } catch (error) {
@@ -64,21 +85,24 @@ async function main() {
 
 main()
   .then(() => {
-    console.log("All child processes completed successfully.");
+    // Forever.
   })
   .catch((error) => {
     console.error(error);
     process.exit(1);
   });
 
-process.on("exit", () => {
-  process.kill(-process.pid, "SIGTERM");
-});
-process.on("SIGINT", () => {
-  process.kill(-process.pid, "SIGINT");
-  process.exit();
-});
-process.on("SIGTERM", () => {
-  process.kill(-process.pid, "SIGTERM");
-  process.exit();
-});
+function cleanupAndExit(signal: NodeJS.Signals) {
+  for (const pid of childPids) {
+    try {
+      process.kill(pid, signal);
+    } catch (err) {
+      // Ignore errors.
+    }
+  }
+  process.exit(signal === "SIGTERM" ? 0 : 1);
+}
+
+process.on("exit", () => cleanupAndExit("SIGTERM"));
+process.on("SIGINT", () => cleanupAndExit("SIGINT"));
+process.on("SIGTERM", () => cleanupAndExit("SIGTERM"));
