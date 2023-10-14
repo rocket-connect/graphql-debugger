fe1a6d68ee67fbd5aa4e4835a7df3a1dd8acb83a
danstarns <danielstarns@hotmail.com>
2023-10-14 10:47:49 +0100

    refactor: remove imgs

--- apps/ui/src/images.tsx
+++ apps/ui/src/images.tsx
@@ -4,8 +4,6 @@ export { default as githubDark } from "../public/github-dark.svg";
 // @ts-ignore
 export { default as npmDark } from "../public/npm-dark.svg";
 // @ts-ignore
-export { default as rocketConnect } from "../public/rocketconnect-icon.svg";
-// @ts-ignore
 export { default as infoFilled } from "../public/info-filled.svg";
 // @ts-ignore
 export { default as infoStroke } from "../public/info-stroke.svg";
