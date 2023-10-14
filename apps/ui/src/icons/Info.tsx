a4ddcfc52fb38c2e5a31bc9b30e43578cc44d679
danstarns <danielstarns@hotmail.com>
2023-10-14 09:17:53 +0100

    Fixing file name casing

--- apps/ui/src/icons/Info.tsx
+++
@@ -1,9 +0,0 @@
-import { infoFilled, infoStroke } from "../images";
-
-export function InfoFilled() {
-  return <img src={infoFilled} alt="info" />;
-}
-
-export function InfoStroke() {
-  return <img src={infoStroke} alt="info" />;
-}
