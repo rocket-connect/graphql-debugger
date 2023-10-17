b1a3a2119e4582113706426bf772169ff394e150
Kreshnik Gashi <kreshnnikgashi@gmail.com>
2023-10-17 13:29:09 +0200

    refactor(ui/view): remove switch statement

--- apps/ui/src/components/sidebar/view.tsx
+++ apps/ui/src/components/sidebar/view.tsx
@@ -2,7 +2,6 @@ import { useContext } from "react";
 
 import { SchemasContext } from "../../context/schemas";
 import { SideBarContext } from "../../context/sidebar";
-import { upperFirst } from "../../utils/upper-first";
 import { SchemaViewer } from "../schema/viewer";
 import { Config } from "./views/config";
 import { Info } from "./views/info/info";
@@ -22,31 +21,34 @@ export function SideBarView() {
   const shouldDisplayLogo = ["login", "info"].includes(viewType);
   const shouldDisplayHeader = !["info"].includes(viewType);
 
-  let description = "";
-
-  switch (sidebarContext?.view?.type) {
-    case "schema":
-      if (schemasContext?.schemaRef.current) {
-        description = "Your GraphQL Schema, with analytics and traces";
-      } else {
-        description = "Select a GraphQL Schema to start exploring your traces";
-      }
-      break;
-    case "info":
-      // Nothing to do here - we just display the logo
-      break;
-    case "config":
-      description =
-        "Configure GraphQL debugger settings, stored in your browser";
-      break;
-    case "login":
-      description =
-        "Access GraphQL Debugger Cloud to view your projects and collaborate with your team";
-      break;
-    default:
-      description = "";
-      break;
-  }
+  const sideBarComponentMapper: Record<
+    string,
+    {
+      description: string;
+      component: JSX.Element;
+    }
+  > = {
+    schema: {
+      description: schemasContext?.schemaRef.current
+        ? "Your GraphQL Schema, with analytics and traces"
+        : "Select a GraphQL Schema to start exploring your traces",
+      component: <SchemaViewer />,
+    },
+    info: {
+      description: "",
+      component: <Info />,
+    },
+    config: {
+      description:
+        "Configure GraphQL debugger settings, stored in your browser",
+      component: <Config />,
+    },
+    login: {
+      description:
+        "Access GraphQL Debugger Cloud to view your projects and collaborate with your team",
+      component: <Login />,
+    },
+  };
 
   return (
     <div className="h-screen flex flex-col items-start w-2/6 max-w-2/6 p-5 text-neutral-100 overflow-scroll gap-5 custom-scrollbar">
@@ -59,18 +61,17 @@ export function SideBarView() {
       )}
       {shouldDisplayHeader ? (
         <div className="flex flex-col gap-2">
-          <h2 className="font-bold">
-            {upperFirst(sidebarContext.view?.type || "")}
+          <h2 className="font-bold first-letter:uppercase">
+            {sidebarContext.view?.type || ""}
           </h2>
-          <p className="text-sm">{description}.</p>
+          <p className="text-sm">
+            {sideBarComponentMapper[viewType].description}.
+          </p>
         </div>
       ) : (
         <></>
       )}
-      {sidebarContext?.view?.type === "schema" ? <SchemaViewer /> : <></>}
-      {sidebarContext?.view?.type === "info" ? <Info /> : <></>}
-      {sidebarContext?.view?.type === "config" ? <Config /> : <></>}
-      {sidebarContext?.view?.type === "login" ? <Login /> : <></>}
+      {sideBarComponentMapper[viewType].component}
     </div>
   );
 }
