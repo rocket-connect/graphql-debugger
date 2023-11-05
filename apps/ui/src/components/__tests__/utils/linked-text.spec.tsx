0bbb1008ce4959ce0514f20caa30c341c691739b
Kreshnik Gashi <kreshnnikgashi@gmail.com>
2023-11-05 23:21:09 +0100

    test(ui): add more assertions for linked-text

--- apps/ui/src/components/__tests__/utils/linked-text.spec.tsx
+++ apps/ui/src/components/__tests__/utils/linked-text.spec.tsx
@@ -1,14 +1,26 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 
 import { LinkedText } from "../../utils/linked-text";
 
 describe("LinkedText", () => {
   it("should render linked text successfully ", () => {
-    const { getByTestId } = render(<LinkedText text="test" href="test" />);
+    const { getByTestId } = render(
+      <LinkedText
+        text="debug queries like a pro"
+        href="https://www.graphql-debugger.com"
+      />,
+    );
 
     expect(getByTestId("linked-text-wrapper")).toBeInTheDocument();
     expect(getByTestId("linked-text-wrapper").tagName).toBe("A");
+    expect(getByTestId("linked-text-wrapper")).toHaveAttribute(
+      "href",
+      "https://www.graphql-debugger.com",
+    );
+    expect(getByTestId("linked-text-wrapper")).toHaveTextContent(
+      "debug queries like a pro",
+    );
   });
   it('should have "underline" class', () => {
     const { getByTestId } = render(<LinkedText text="test" href="test" />);
