8c325229e039c73794607c8adae905635f39db3c
Kreshnik Gashi <kreshnnikgashi@gmail.com>
2023-11-05 23:33:27 +0100

    test(ui): changes

--- apps/ui/src/components/__tests__/utils/linked-text.spec.tsx
+++ apps/ui/src/components/__tests__/utils/linked-text.spec.tsx
@@ -23,13 +23,23 @@ describe("LinkedText", () => {
     );
   });
   it('should have "underline" class', () => {
-    const { getByTestId } = render(<LinkedText text="test" href="test" />);
+    const { getByTestId } = render(
+      <LinkedText
+        text="debug queries like a pro"
+        href="https://www.graphql-debugger.com"
+      />,
+    );
 
     expect(getByTestId("linked-text-wrapper")).toBeInTheDocument();
     expect(getByTestId("linked-text-wrapper")).toHaveClass("underline");
   });
   it("should be bold", () => {
-    const { getByTestId } = render(<LinkedText text="test" href="test" />);
+    const { getByTestId } = render(
+      <LinkedText
+        text="debug queries like a pro"
+        href="https://www.graphql-debugger.com"
+      />,
+    );
 
     expect(getByTestId("linked-text-wrapper")).toBeInTheDocument();
     expect(getByTestId("linked-text-wrapper")).toHaveClass("font-bold");
