76276d857e91e2576b707792becd7f49393f7f2f
Kreshnik Gashi <kreshnnikgashi@gmail.com>
2023-11-08 12:55:24 +0100

    feat(ui): add theme_type as const

--- apps/ui/src/utils/constants.ts
+++ apps/ui/src/utils/constants.ts
@@ -1,6 +1,6 @@
 import type { Theme } from "../store/useThemeStore";
 
-export const THEME_TYPE: Record<Theme, string> = {
+export const THEME_TYPE: Record<Theme, Theme> = {
   light: "light",
   dark: "dark",
 };
