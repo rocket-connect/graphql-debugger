c17a45e788d823961cdbb4665ea84e34d792626f
Kreshnik Gashi <kreshnnikgashi@gmail.com>
2023-11-08 13:28:20 +0100

    refactor(ui/toggle): conditionally render classnames with cn function

--- apps/ui/src/components/utils/toggle.tsx
+++ apps/ui/src/components/utils/toggle.tsx
@@ -2,6 +2,8 @@ import classNames from "classnames";
 import { ChangeEvent, useState } from "react";
 import toast from "react-hot-toast";
 
+import { cn } from "../../utils/cn";
+
 interface ToggleProps {
   initialState?: boolean;
   onToggle: (isChecked: boolean) => void;
@@ -65,11 +67,10 @@ export function Toggle({
             readOnly
           />
           <div
-            className={classNames(
-              `w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-green-300  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all `,
+            className={cn(
+              "w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-green-300  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-graphql-otel-green",
               {
-                "peer-checked:bg-app-blue": blueToggle,
-                "peer-checked:bg-graphql-otel-green": !blueToggle,
+                ["peer-checked:bg-app-blue"]: blueToggle,
               },
             )}
           ></div>
