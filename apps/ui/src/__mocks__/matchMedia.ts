bb83523b0492314f2c57d7b4f6165c9ae47fdb75
Kreshnik Gashi <kreshnnikgashi@gmail.com>
2023-11-23 01:01:48 +0100

    test(ui): revert back deprecated methods, as tests are failing

--- apps/ui/src/__mocks__/matchMedia.ts
+++ apps/ui/src/__mocks__/matchMedia.ts
@@ -4,6 +4,8 @@ Object.defineProperty(window, "matchMedia", {
     matches: false,
     media: query,
     onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
     addEventListener: jest.fn(),
     removeEventListener: jest.fn(),
     dispatchEvent: jest.fn(),
