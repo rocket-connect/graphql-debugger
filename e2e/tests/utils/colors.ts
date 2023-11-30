a556f37a757b73491f0decfd42392dd32362c37a
danstarns <danielstarns@hotmail.com>
2023-11-30 21:05:38 +0700

    fix: pin colors for light mode on e2e

--- e2e/tests/utils/colors.ts
+++ e2e/tests/utils/colors.ts
@@ -1,10 +1,10 @@
 export const colors = {
-  green_text: "rgb(43, 166, 119)",
+  green_text: "rgb(43, 171, 124)",
   green_background: "rgba(43, 166, 119, 0.3)",
 
   red_text: "rgb(239, 67, 67)",
   red_background: "rgba(239, 68, 68, 0.3)",
 
-  netural_text: "rgb(183, 194, 215)",
+  netural_text: "rgb(59, 76, 104)",
   netural_background: "rgba(183, 194, 215, 0.3)",
 };
