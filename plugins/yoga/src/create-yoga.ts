9b15f0f79cfffe9b0de3245c84423c50f3fa9dac
danstarns <danielstarns@hotmail.com>
2023-12-01 13:29:21 +0700

    feat: add yoga context and schema wrappers

--- plugins/yoga/src/create-yoga.ts
+++ plugins/yoga/src/create-yoga.ts
@@ -1,5 +1,11 @@
-import { TraceSchemaInput } from "@graphql-debugger/trace-schema";
+import {
+  GraphQLOTELContext,
+  GraphQLOTELContextOptions,
+  TraceSchemaInput,
+  traceSchema,
+} from "@graphql-debugger/trace-schema";
 
+import { GraphQLSchema } from "graphql";
 import {
   YogaServerInstance,
   YogaServerOptions,
@@ -8,16 +14,62 @@ import {
 
 interface DebuggerYogaOptions<TServerContext, TUserContext>
   extends YogaServerOptions<TServerContext, TUserContext> {
-  debugger: Omit<TraceSchemaInput, "schema">;
+  debugger?: Omit<TraceSchemaInput, "schema"> & {
+    GraphQLOTELContextOptions?: GraphQLOTELContextOptions;
+  };
+  schema: GraphQLSchema;
+  context?: (req: any) => Promise<TUserContext>;
 }
 
+/*
+ * This is a wrapper of createYoga function from graphql-yoga
+ * with the addition of the traceSchema function. This function modifies the context.
+ */
 export function createYoga<
   TServerContext extends Record<string, any> = {},
-  TUserContext extends Record<string, any> = {},
+  TUserContext extends Record<string, any> & {
+    GraphQLOTELContext?: GraphQLOTELContext;
+  } = {},
 >(
   options: DebuggerYogaOptions<TServerContext, TUserContext>,
 ): YogaServerInstance<TServerContext, TUserContext> {
-  const yoga = originalCreateYoga<TServerContext, TUserContext>(options);
+  if (!options.schema) {
+    throw new Error("Schema is required");
+  }
+
+  if (!options.context) {
+    const contextOverride = async (): Promise<TUserContext> => {
+      return {
+        GraphQLOTELContext: new GraphQLOTELContext(
+          options?.debugger?.GraphQLOTELContextOptions,
+        ),
+      } as unknown as TUserContext;
+    };
+
+    options.context = contextOverride;
+  }
+
+  if (typeof options.context === "function") {
+    const originalContextFunction = options.context;
+    options.context = async (...args) => {
+      const contextObject = await originalContextFunction(...args);
+
+      contextObject.GraphQLOTELContext = new GraphQLOTELContext(
+        options?.debugger?.GraphQLOTELContextOptions,
+      );
+
+      return contextObject;
+    };
+  }
+
+  const tracedSchema = traceSchema({
+    schema: options.schema,
+  });
+
+  const yoga = originalCreateYoga<TServerContext, TUserContext>({
+    ...options,
+    schema: tracedSchema,
+  });
 
   return yoga;
 }
