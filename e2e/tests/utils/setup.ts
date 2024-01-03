f7cd426b2a7af6fce24fa466c990df84c068f955
Sam Laycock <93922969+samlaycock@users.noreply.github.com>
2024-01-03 17:37:09 +0000

    feat: add Dockerfile (#154)
    * feat: add Dockerfile
    
    * docs: add inital for docker
    
    * docs: *
    
    * ci: add init for docker e2e
    
    * fix: remove typo - lol
    
    * ci: env debug
    
    * ci(temp): only run one db touching test
    
    * ci(temp): debug tests for docker
    
    * docker: add db to ignore
    
    * ci: change debug to runtime to not get build debug
    
    * ci: log only debugger debug
    
    * test: mount data access sqlite DB into Docker container on test run
    
    * typo
    
    * fix: changes to dev.db premissions
    
    * fix: remove skipped tests
    
    * fix: remove logs from e2e
    
    * ci: speed up e2e
    
    ---------
    
    Co-authored-by: Daniel Starns <danielstarns@hotmail.com>

--- e2e/tests/utils/setup.ts
+++ e2e/tests/utils/setup.ts
@@ -2,14 +2,21 @@ import { clearDB } from "@graphql-debugger/data-access";
 
 import * as backend from "./backend";
 
+const shouldSpawnBackend = process.env.E2E_IN_DOCKER !== "true";
+
 beforeAll(async () => {
-  await backend.listen();
+  if (shouldSpawnBackend) {
+    await backend.listen();
+  }
 });
 
 beforeEach(async () => {
+  // TODO - for docker we might have to add an api to clear the db as this will point to the host machine
   await clearDB();
 });
 
 afterAll(async () => {
-  await backend.close();
+  if (shouldSpawnBackend) {
+    await backend.close();
+  }
 });
