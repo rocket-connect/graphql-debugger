e66831d1a9c39c00612a260c339ddf49e3096070
danstarns <danielstarns@hotmail.com>
2024-01-06 10:29:30 +0000

    refactor: use dbspantonetwork in sqlite adapter

--- packages/adapters/sqlite/src/entities/Span.ts
+++ packages/adapters/sqlite/src/entities/Span.ts
@@ -1,10 +1,10 @@
 import { BaseSpan } from "@graphql-debugger/adapter-base";
 import { prisma } from "@graphql-debugger/data-access";
-import { TimeStamp, UnixNanoTimeStamp } from "@graphql-debugger/time";
 import {
   AggregateSpansResponse,
   AggregateSpansWhere,
 } from "@graphql-debugger/types";
+import { dbSpanToNetwork } from "@graphql-debugger/utils";
 
 export class SQLiteSpan extends BaseSpan {
   constructor() {
@@ -30,34 +30,7 @@ export class SQLiteSpan extends BaseSpan {
       errorCount: 0,
       averageDuration: "0",
       lastResolved: "0",
-      spans: spans.map((span) => {
-        return {
-          id: span.id,
-          spanId: span.spanId,
-          parentSpanId: span.parentSpanId as string,
-          traceId: span.traceId,
-          name: span.name as string,
-          kind: span.kind,
-          startTimeUnixNano: new UnixNanoTimeStamp(
-            span.startTimeUnixNano,
-          ).toString(),
-          endTimeUnixNano: new UnixNanoTimeStamp(
-            span.endTimeUnixNano,
-          ).toString(),
-          durationNano: new UnixNanoTimeStamp(span.durationNano).toString(),
-          graphqlDocument: span.graphqlDocument,
-          graphqlVariables: span.graphqlVariables,
-          graphqlResult: span.graphqlResult,
-          graphqlContext: span.graphqlContext,
-          timestamp: 0,
-          createdAt: new TimeStamp(span.createdAt).toString(),
-          updatedAt: new TimeStamp(span.updatedAt).toString(),
-          errorMessage: span.errorMessage,
-          errorStack: span.errorStack,
-          isForeign: span.isForeign,
-          attributes: span.attributes as string,
-        };
-      }),
+      spans: spans.map((span) => dbSpanToNetwork(span)),
     };
   }
 }
