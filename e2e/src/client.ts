1a587a466b6c1f8b715a49f6dd880ca5991c2977
danstarns <danielstarns@hotmail.com>
2024-01-12 14:09:14 +0000

    feat: add matrix to e2e tests

--- e2e/src/client.ts
+++ e2e/src/client.ts
@@ -1,7 +1,26 @@
+import { BaseAdapter } from "@graphql-debugger/adapter-base";
+import { ProxyAdapter } from "@graphql-debugger/adapter-proxy";
 import { SQLiteAdapter } from "@graphql-debugger/adapter-sqlite";
 import { DebuggerClient } from "@graphql-debugger/client";
 
-const adapter = new SQLiteAdapter();
+const ADAPTER_TYPE: string = process.env.ADAPTER_TYPE ?? "sqlite";
+
+let adapter: BaseAdapter;
+
+switch (ADAPTER_TYPE) {
+  case "sqlite":
+    adapter = new SQLiteAdapter();
+    break;
+
+  case "proxy":
+    adapter = new ProxyAdapter({
+      apiURL: "http://localhost:16686",
+      collectorURL: "http://localhost:4318",
+    });
+    break;
+  default:
+    throw new Error(`Unknown adapter type: ${ADAPTER_TYPE}`);
+}
 
 export const client = new DebuggerClient({
   adapter,
