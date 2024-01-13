43764bb506df79d861b3a997fbdf980484dd0020
danstarns <danielstarns@hotmail.com>
2024-01-13 23:55:16 +0000

    refactor: remove graphql schema tests

--- packages/graphql-schema/tests/client.ts
+++
@@ -1,8 +0,0 @@
-import { SQLiteAdapter } from "@graphql-debugger/adapter-sqlite";
-import { DebuggerClient } from "@graphql-debugger/client";
-
-const adapter = new SQLiteAdapter();
-
-export const client = new DebuggerClient({
-  adapter,
-});
