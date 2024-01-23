391ee77862fc258d2fe0767d208ba278c3c55acd
danstarns <danielstarns@hotmail.com>
2024-01-23 11:40:32 +0000

    seo: init work

--- docs/src/pages/_document.tsx
+++ docs/src/pages/_document.tsx
@@ -1,9 +1,12 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <GoogleAnalytics gaId="G-KWK6N3X1ZD" />
+      </Head>
       <body>
         <Main />
         <NextScript />
