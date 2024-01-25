bc8e330691bcdacf5a9f32eaef2783e59b13e269
Daniel Starns <danielstarns@hotmail.com>
2024-01-25 15:10:18 +0000

    google analytics docs (#213)
    * refactor: remove next/third-parties
    
    * feat: use nextjs-google-analytics

--- docs/src/pages/_app.tsx
+++ docs/src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GoogleAnalytics gaMeasurementId="G-KWK6N3X1ZD" trackPageViews />
+      <Component {...pageProps} />
+    </>
+  );
 }
--- docs/src/pages/_document.tsx
+++ docs/src/pages/_document.tsx
@@ -1,12 +1,9 @@
-import { GoogleAnalytics } from "@next/third-parties/google";
 import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <GoogleAnalytics gaId="G-KWK6N3X1ZD" />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
