cb18593e575d22f56eaa2948916cd866bdff714f
danstarns <danielstarns@hotmail.com>
2024-04-27 17:17:51 +0700

    package: remove safe-json-stringify

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,6 +1,5 @@
 export * from "./debug";
 export * from "./hash-schema";
-export * from "./safe-json";
 export * from "./sum-trace-time";
 export * from "./db-span-to-network";
 export * from "./get-trace-start";
