4e303a8613206738195a79fbea6edd914790efc8
danstarns <danielstarns@hotmail.com>
2024-04-30 16:53:30 +0700

    init: benchmarks

---
+++ benchmarks/apollo-debugger/index.js
@@ -0,0 +1,29 @@
+import { graphqlDebuggerPlugin } from "@graphql-debugger/apollo-server";
+
+import { ApolloServer } from "apollo-server";
+import cluster from "cluster";
+import { cpus } from "os";
+
+process.env.PORT = "8000";
+
+if (cluster.isPrimary) {
+  for (let i = 0; i < cpus().length; i++) {
+    cluster.fork();
+  }
+} else {
+  const server = new ApolloServer({
+    typeDefs: `
+        type Query {
+          hello: String!
+        }
+      `,
+    resolvers: {
+      Query: {
+        hello: () => "world",
+      },
+    },
+    plugins: [graphqlDebuggerPlugin()],
+  });
+
+  server.listen(8000);
+}
