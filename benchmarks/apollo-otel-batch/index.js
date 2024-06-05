9ce33126b2a01e27c507a3fbcb99757eced75c20
danstarns <danielstarns@hotmail.com>
2024-06-05 11:59:28 +0100

    feat: add apollo-otel-batch

---
+++ benchmarks/apollo-otel-batch/index.js
@@ -0,0 +1,53 @@
+import opentelemetry from "@opentelemetry/api";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { Resource } from "@opentelemetry/resources";
+import {
+  BatchSpanProcessor,
+  SimpleSpanProcessor,
+} from "@opentelemetry/sdk-trace-base";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+import { ApolloServer } from "apollo-server";
+import cluster from "cluster";
+import { cpus } from "os";
+
+process.env.PORT = "8000";
+
+const provider = new BatchSpanProcessor({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: "basic-service",
+  }),
+});
+
+const exporter = new OTLPTraceExporter({});
+provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+
+provider.register();
+
+if (cluster.isPrimary) {
+  for (let i = 0; i < cpus().length; i++) {
+    cluster.fork();
+  }
+} else {
+  const server = new ApolloServer({
+    typeDefs: `
+        type Query {
+          hello: String!
+        }
+      `,
+    resolvers: {
+      Query: {
+        hello: (root, args, context, info) => {
+          const tracer = opentelemetry.trace.getTracer("example-tracer");
+          const span = tracer.startSpan("say-hello");
+          span.setAttribute("hello-to", "world");
+          span.setAttribute("query", JSON.stringify(info.operation));
+          span.addEvent("invoking resolvers");
+          span.end();
+          return "world";
+        },
+      },
+    },
+  });
+
+  server.listen(8000);
+}
